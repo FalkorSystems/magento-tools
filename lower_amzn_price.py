155e1d584727fdac13dbf848a9128dc26495aabf
Sameer Parekh <sameer@iheartengineering.com>
2013-09-11 18:05:28 +0000

    init

---
+++ lower_amzn_price.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Lower amazon prices for everything by 10%
+
+import xmlrpclib
+import yaml
+import csv
+
+server = xmlrpclib.ServerProxy( "http://store.iheartengineering.com/index.php/api/xmlrpc" )
+session = server.login( 'api', 'testtest1' )
+
+# get product list
+product_list = server.call( session, 'catalog_product.list' )
+
+for product in product_list:
+    sku = product['sku']
+    try:
+        product_info = server.call( session, 'catalog_product.info', sku )
+    except:
+        print "Warning: unable to get product %s" % sku
+    else:
+        # Check amazon price list first, otherwise
+        try:
+            price = float(product_info['amazon_price'])
+        except TypeError as e:
+            print "Error: Amazon price for %s is %s" % (sku, product_info['amazon_price'])
+        else:
+            amazon_price = price * 0.90
+
+            print "Setting amazon price for %s to %4.2f from %4.2f" % ( sku, amazon_price,
+                                                                        price )
+            parms = [sku, {'amazon_price': "%4.2f" % amazon_price}]
+            result = server.call( session, 'catalog_product.update', parms )
